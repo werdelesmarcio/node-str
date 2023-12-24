a0201f7d147990b45c84a1d104f0a7ae7ccfe62b
Werdeles Soares <werdelesmarcio@hotmail.com>
2023-12-24 03:08:52 -0300

    Atualizacao

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -2,24 +2,15 @@
 
 const express = require("express");
 const router = express.Router();
+const controller = require('../controllers/product-controller');
 
 // Rota post
-router.post("/", (req, res, next) => {
-  res.status(201).send(req.body);
-});
+router.post("/", controller.post);
 
 // Rota put
-router.put("/:id", (req, res, next) => {
-  const id = req.params.id;
-  res.status(201).send({
-    id: id,
-    item: req.body,
-  });
-});
+router.put("/:id", controller.put);
 
 // Rota delete
-router.delete("/", (req, res, next) => {
-  res.status(200).send(req.body);
-});
+router.delete("/", controller.delete);
 
 module.exports = router;
